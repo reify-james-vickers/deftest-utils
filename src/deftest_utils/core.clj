38421758c7f16e454f1887abe9502c340764ccb7
reify-james-vickers <james.vickers@reifyhealth.com>
2022-10-18 11:59:48 -0600

    Change macro to do both timeout and retry

--- src/deftest_utils/core.clj
+++ src/deftest_utils/core.clj
@@ -1,23 +1,37 @@
 (ns deftest-utils.core
   (:require [clojure.spec.alpha :as s]
-            [diehard.core :as dh]))
+            [diehard.core]))
 
-(s/def ::timeout-ms (s/and int? pos?))
-(s/def ::deftest-timed-config
+(s/def ::pos-int (s/and int? pos?))
+
+(s/def ::timeout-ms ::pos-int)
+(s/def ::timeout
   (s/keys :req-un [::timeout-ms]))
 
-(defmacro deftest-timed
-  [{:keys [timeout-ms] :as config} & body]
+(s/def ::max-retries ::pos-int)
+(s/def ::retry-on symbol?)
+(s/def ::retry
+  (s/keys :opt-un [::max-retries
+                   ::retry-on]))
+(s/def ::deftest-timed-config
+  (s/keys :opt-un [::retry
+                   ::timeout]))
+
+(defmacro deftest-2
+  [{:keys [retry timeout] :as config} & body] ; TODO diehard default retries is infinite (?), should probably default to like 3 here.
   (when-let [info (s/explain-data ::deftest-timed-config config)]
     (throw (Exception. (str info))))
-  (let [base-config# {:interrupt? true}]
+  (let [test-body# (-> body rest first) ; remove test name var
+        decorated-body# (cond->> test-body#
+                          timeout (list 'diehard.core/with-timeout timeout)
+                          retry (list 'diehard.core/with-retry retry))]
     `(clojure.test/deftest ~(first body) ; var name of test
        (try ; wrap in try-catch to give better test output on failure, don't need diehard stack trace.
-         (dh/with-timeout (merge ~base-config# ~config)
-           ~@(rest body))
+         ~decorated-body#
          (catch dev.failsafe.TimeoutExceededException e#
-           (clojure.test/is false (clojure.core/format "Test timed out after %s ms" ~timeout-ms)))))))
+           (clojure.test/is false (clojure.core/format "Test timed out after %s ms" ~(:timeout-ms timeout))))))))
 
 (comment
-  (macroexpand-1 '(deftest-timed {:timeout-ms 5 :foo "bar"} some-test (let [x 5])))
-  (macroexpand '(deftest-timed {:timeout-ms 5} some-test (let [x 5]))))
+  (macroexpand-1 '(deftest-2 {:timeout {:timeout-ms 5} :retry {:retry-on Exception :max-retries 3}}
+                    some-test (let [x 5] (println x) (do (+ x 5)))))
+  (macroexpand '(deftest-2 {:timeout-ms 5} some-test (let [x 5]))))
--- test/deftest_utils/core_test.clj
+++ test/deftest_utils/core_test.clj
@@ -1,9 +1,17 @@
 (ns deftest-utils.core-test
-  (:require [clojure.test :refer [testing]]
-            [deftest-utils.core :refer [deftest-timed]]))
+  (:require [clojure.test :refer [is testing]]
+            [deftest-utils.core :refer [deftest-2]]))
 
-(deftest-timed {:timeout-ms 500} foo
+(deftest-2 {:timeout {:timeout-ms 500}} test-timeout-setup
   (testing "hi"
-    (let [x 5]
-      (println "hello")
-      (Thread/sleep 10000))))
+    (Thread/sleep 10000)))
+
+(def tries (atom 0))
+
+(deftest-2 {:retry {:max-retries 20}} test-retry-setup
+  (testing "hi"
+    (if (< (swap! tries inc) 10)
+      (do
+        (println (str "failing on try " @tries))
+        (throw (IllegalArgumentException. (str "failing on try " @tries))))
+      (is true))))
