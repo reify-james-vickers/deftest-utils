aa8402f2228bb7b695e0742400be97b3732a1c24
reify-james-vickers <james.vickers@reifyhealth.com>
2022-10-18 13:03:39 -0600

    Make timeout map take :millis instead of :timeout-ms

--- src/deftest_utils/core.clj
+++ src/deftest_utils/core.clj
@@ -1,12 +1,13 @@
 (ns deftest-utils.core
   (:require [clojure.spec.alpha :as s]
+            [clojure.set]
             [diehard.core]))
 
 (s/def ::pos-int (s/and int? pos?))
 
-(s/def ::timeout-ms ::pos-int)
+(s/def ::millis ::pos-int)
 (s/def ::timeout
-  (s/keys :req-un [::timeout-ms]))
+  (s/keys :req-un [::millis]))
 
 (s/def ::max-retries ::pos-int)
 (s/def ::retry-on symbol?)
@@ -23,15 +24,15 @@
     (throw (Exception. (str info))))
   (let [test-body# (-> body rest first) ; remove test name var
         decorated-body# (cond->> test-body#
-                          timeout (list 'diehard.core/with-timeout timeout)
+                          timeout (list 'diehard.core/with-timeout (clojure.set/rename-keys timeout {:millis :timeout-ms}))
                           retry (list 'diehard.core/with-retry retry))]
     `(clojure.test/deftest ~(first body) ; var name of test
        (try ; wrap in try-catch to give better test output on failure, don't need diehard stack trace.
          ~decorated-body#
          (catch dev.failsafe.TimeoutExceededException e#
-           (clojure.test/is false (clojure.core/format "Test timed out after %s ms" ~(:timeout-ms timeout))))))))
+           (clojure.test/is false (clojure.core/format "Test timed out after %s ms" ~(:millis timeout))))))))
 
 (comment
-  (macroexpand-1 '(deftest-2 {:timeout {:timeout-ms 5} :retry {:retry-on Exception :max-retries 3}}
+  (macroexpand-1 '(deftest-2 {:timeout {:millis 5} :retry {:retry-on Exception :max-retries 3}}
                     some-test (let [x 5] (println x) (do (+ x 5)))))
   (macroexpand '(deftest-2 {:timeout-ms 5} some-test (let [x 5]))))
